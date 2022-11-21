3efdb456c6864c1b3e0a1d6d2f89e779d86066f9
LISA <ASUS@LAPTOP-67G15PD7>
2022-11-21 16:17:18 +0700

    add public bool dellNode

--- Exercise_3/Program.cs
+++ Exercise_3/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,9 +10,10 @@ namespace Exercise_3
     class Node
     {
         /*creates Nodes for the circular nexted list*/
-        public int rollNumber;
+        public int NoMhs;
         public string name;
         public Node next;
+        public Node prev;
     }
     class CircularList
     {
@@ -20,6 +22,59 @@ namespace Exercise_3
         public CircularList()
         {
             LAST = null;
+        }
+        public void addNode()
+        {
+            int nim;
+            string nm;
+            Console.WriteLine("\nEnter the roll number of the student: ");
+            nim = Convert.ToInt32(Console.ReadLine());
+            Console.Write("\nEnter the name of the student: ");
+            nm = Console.ReadLine();
+            Node newNode = new Node();
+            newNode.noMhs = nim;
+            newNode.name = nm;
+
+            //check if the list empty
+            if (LAST == null || nim <= LAST.NoMhs)
+            {
+                if ((LAST != null) && (nim == LAST.NoMhs))
+                {
+                    Console.WriteLine("\nDuplicate number not allowed");
+                    return;
+                }
+                newNode.next = LAST;
+                if (LAST != null)
+                    LAST.prev = newNode;
+                newNode.prev = null;
+                LAST = newNode;
+                return;
+            }
+            /* if the node is tobe inserted at between two node */
+            Node previous, current;
+            for (current = previous = LAST; current != null && nim >= current.noMhs; previous = current, current = current.next)
+            {
+                if (nim == current.noMhs)
+                {
+                    Console.WriteLine("\nDuplicate roll numbers not allowed");
+                    return;
+                }
+            }
+            /* On the execution of the above for loop, prev and
+             * * current will point to those nodes
+             * * between wich the new node is to be insarted */
+            newNode.next = current;
+            newNode.prev = previous;
+
+            //if the node is to be insarted at the end of the list
+            if (current == null)
+            {
+                newNode.next = null;
+                previous.next = newNode;
+                return;
+            }
+            current.prev = newNode;
+            previous.next = newNode;
         }
         public bool Search(int rollNo, ref Node previous, ref Node current)/*Seaches for the specofoed node*/
         {
@@ -32,6 +87,32 @@ namespace Exercise_3
                 return true;
             else
                 return (false); /*returns false if the node is not found*/
+        }
+
+        public bool dellNode(int rollNo)
+        {
+            Node previous, current;
+            previous = current = null;
+            if (search(rollNo, ref previous, ref current) == false)
+                return false;
+            //the begining of data
+            if (current.next == null)
+            {
+                previous.next = null;
+                return true;
+            }
+            //node between two nodes in the list
+            if (current == LAST)
+            {
+                LAST = LAST.next;
+                if (LAST != null)
+                    LAST.prev = null;
+                return true;
+            }
+            /*if the to be deleted is in between the list then the following lines of executed. */
+            previous.next = current.next;
+            current.next.prev = previous;
+            return true;
         }
         public bool listEmpty()
         {
@@ -69,6 +150,65 @@ namespace Exercise_3
     {
         static void Main(string[] args)
         {
+            CircularList obj = new CircularList();
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("\nMenu");
+                    Console.WriteLine("1. View all the records in the list");
+                    Console.WriteLine("2. Search for a record in the list");
+                    Console.WriteLine("3. Display the first record in the list");
+                    Console.WriteLine("4. Exit");
+                    Console.Write("\nEnter your choice (1-4) : ");
+                    char ch = Convert.ToChar(Console.ReadLine());
+                    switch (ch)
+                    {
+                        case '1':
+                            {
+                                obj.traverse();
+                            }
+                            break;
+                        case '2':
+                            {
+                                if (obj.listEmpty() == true)
+                                {
+                                    Console.WriteLine("\nList is Empty");
+                                    break;
+                                }
+                                Node prev, curr;
+                                prev = curr = null;
+                                Console.Write("\nEnter the roll number of the student whose record is to be searched: ");
+                                int num = Convert.ToInt32(Console.ReadLine());
+                                if (obj.Search(num, ref prev, ref curr) == false)
+                                    Console.WriteLine("\nRecord not found");
+                                else
+                                {
+                                    Console.WriteLine("\nRecord found");
+                                    Console.WriteLine("\nRoll number: " + curr.rollNumber);
+                                    Console.WriteLine("\nName: " + curr.name);
+                                }
+                            }
+                            break;
+                        case '3':
+                            {
+                                obj.firstNode();
+                            }
+                            break;
+                        case '4':
+                            return;
+                        default:
+                            {
+                                Console.WriteLine("Invalid Option");
+                                break;
+                            }
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.ToString());
+                }
+            }
         }
     }
 }
