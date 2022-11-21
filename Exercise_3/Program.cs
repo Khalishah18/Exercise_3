f4451139d9565d2ccf78e1567b0310292e4c3ca3
LISA <ASUS@LAPTOP-67G15PD7>
2022-11-21 16:45:27 +0700

    membenarkan dan menambahkan code yang kurang

--- Exercise_3/Program.cs
+++ Exercise_3/Program.cs
@@ -11,9 +11,10 @@ namespace Exercise_3
     {
         /*creates Nodes for the circular nexted list*/
         public int NoMhs;
+        public int rollNumber;
         public string name;
-        public Node next;
-        public Node prev;
+        public Node next; //point to the succeding node
+        public Node prev; //point to the preccending node
     }
     class CircularList
     {
@@ -23,97 +24,63 @@ namespace Exercise_3
         {
             LAST = null;
         }
-        public void addNode()
+        public void addNode() //add a node in the list
         {
-            int nim;
+            int rollNumber;
             string nm;
             Console.WriteLine("\nEnter the roll number of the student: ");
-            nim = Convert.ToInt32(Console.ReadLine());
-            Console.Write("\nEnter the name of the student: ");
+            rollNumber = Convert.ToInt32(Console.ReadLine());
+            Console.Write("\nEnter the roll name of the student: ");
             nm = Console.ReadLine();
-            Node newNode = new Node();
-            newNode.noMhs = nim;
-            newNode.name = nm;
+            Node newnode = new Node();
+            newnode.rollNumber = rollNumber;
+            newnode.name = nm;
 
-            //check if the list empty
-            if (LAST == null || nim <= LAST.NoMhs)
+            //if the node to be inserted is the first node
+            if ((LAST == null) || rollNumber <= LAST.rollNumber)
             {
-                if ((LAST != null) && (nim == LAST.NoMhs))
+                if ((LAST != null) && (rollNumber == LAST.rollNumber))
                 {
-                    Console.WriteLine("\nDuplicate number not allowed");
+                    Console.WriteLine();
                     return;
                 }
-                newNode.next = LAST;
-                if (LAST != null)
-                    LAST.prev = newNode;
-                newNode.prev = null;
-                LAST = newNode;
+                newnode.next = LAST;
+                LAST = newnode;
                 return;
             }
-            /* if the node is tobe inserted at between two node */
+
             Node previous, current;
-            for (current = previous = LAST; current != null && nim >= current.noMhs; previous = current, current = current.next)
+            previous = LAST;
+            current = LAST;
+
+            while ((current != null) && (rollNumber >= current.rollNumber))
             {
-                if (nim == current.noMhs)
+                if (rollNumber == current.rollNumber)
                 {
-                    Console.WriteLine("\nDuplicate roll numbers not allowed");
+                    Console.WriteLine();
                     return;
                 }
+                previous.next = current;
+                previous.next = newnode;
             }
-            /* On the execution of the above for loop, prev and
-             * * current will point to those nodes
-             * * between wich the new node is to be insarted */
-            newNode.next = current;
-            newNode.prev = previous;
-
-            //if the node is to be insarted at the end of the list
+            newnode.next = current;
+            previous.next = newnode;
+        }
+        public bool Search(int rollNumber, ref Node previous, ref Node current)/*Seaches for the specofoed node*/
+        {
+            previous = LAST;
+            current = LAST;
+            while ((current != null) && (rollNumber >= current.rollNumber))
+            {
+                previous = current;
+                current = current.next;
+            }
             if (current == null)
-            {
-                newNode.next = null;
-                previous.next = newNode;
-                return;
-            }
-            current.prev = newNode;
-            previous.next = newNode;
-        }
-        public bool Search(int rollNo, ref Node previous, ref Node current)/*Seaches for the specofoed node*/
-        {
-            for (previous = current = LAST.next; current != LAST; previous = current, current = current.next)
-            {
-                if (rollNo == current.rollNumber)
-                    return (true); /* returns true if the node is found */
-            }
-            if (rollNo == LAST.rollNumber) /* if the node is present at the end*/
-                return true;
+                return false;
             else
-                return (false); /*returns false if the node is not found*/
+                return true;
         }
 
-        public bool dellNode(int rollNo)
-        {
-            Node previous, current;
-            previous = current = null;
-            if (search(rollNo, ref previous, ref current) == false)
-                return false;
-            //the begining of data
-            if (current.next == null)
-            {
-                previous.next = null;
-                return true;
-            }
-            //node between two nodes in the list
-            if (current == LAST)
-            {
-                LAST = LAST.next;
-                if (LAST != null)
-                    LAST.prev = null;
-                return true;
-            }
-            /*if the to be deleted is in between the list then the following lines of executed. */
-            previous.next = current.next;
-            current.next.prev = previous;
-            return true;
-        }
         public bool listEmpty()
         {
             if (LAST == null)
@@ -146,7 +113,7 @@ namespace Exercise_3
                 Console.WriteLine("\nThe first record in the list is: \n\n " + LAST.next.rollNumber + "" + LAST.next.name);
         }
     }
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
